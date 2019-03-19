eade5058ff0946c20454de7d01db41fee816a6b6
Fredy B <fredy.ballesteros@gmail.com>
2019-03-19 17:45:26 -0500

    Configura BD

--- prodjango/webplayground/webplayground/settings.py
+++ prodjango/webplayground/webplayground/settings.py
@@ -81,11 +81,8 @@ WSGI_APPLICATION = 'webplayground.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'HOST': 'db',
-        'PORT': 5432,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
